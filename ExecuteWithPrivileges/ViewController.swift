99fd493dd7aaf3a150284de7b1a173ec88afa2d3
Duff Neubauer <dneubauer@apple.com>
2015-12-10 19:46:33 -0800

    Initial Commit

---
+++ ExecuteWithPrivileges/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExecuteWithPrivileges
+//
+//  Created by Duff Neubauer on 12/10/15.
+//  Copyright © 2015 Duff Neubauer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
